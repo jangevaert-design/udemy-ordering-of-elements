dc274672e0151a273c0360b2faf24ac93b2fe209
Jan Gevaert <jan.gevaert@gmail.com>
2021-07-11 10:25:29 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/OrderingOfElements.java
@@ -0,0 +1,18 @@
+//import java.util.*; Not here!
+
+package edu.cnm.deepdive;
+
+//public class AnotherClass {} Only one public class allowed inside one Java file.
+
+public class OrderingOfElements {
+
+//  package edu.cnm.deepdive; not here!
+  //import java.util.*; Not here!
+
+  public static void main(String[] args) {
+  //void anotherMethod() {} No method allowed in another method.
+  }
+
+}
+
+//public void myMethod() {} Not here!
